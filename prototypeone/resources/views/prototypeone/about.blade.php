9cc930b88c53f1b46f54c3f2c84686b2c5770400
RandomTroll18 <joshuaarnoldrillera@yahoo.com.ph>
2015-05-11 23:58:52 +0000

    Changed about page and register home page.

--- prototypeone/resources/views/prototypeone/about.blade.php
+++ prototypeone/resources/views/prototypeone/about.blade.php
@@ -6,9 +6,47 @@
 	<div class="container">
 		<h1>About This Prototype</h1>
 		<p>
-		This is the prototype for the Sprint Zero presentation. This took a lot of work to get setup over the 'break'. 
-		Please give us a good mark for this
+		This is the prototype which was made after the first progress 
+		report for this semester. There is much more functionality 
+		in this prototype (compared to the 
+		previous prototype) like the following:
 		</p>
+		{!! HTML::ul(array(
+			"Multiple User Accounts" => array(
+				"AT Professional" => array(
+					"A user who is a member of the panel system", 
+					"User's purpose is to review the AT Seeker's 
+						research notes."
+				), 
+				"AT Seeker" => array(
+					"A user who is looking for Assistive Technology",
+					"This user can create research notes and 
+					submit these research notes to be reviewed by 
+					a panel of AT experts"
+				), 
+				"AT Supplier" => array(
+					"A user who suppliers Assistive Technology", 
+					"This user provides information on the 
+					Assistive Technology they supply, which 
+					should include the cost"
+				), 
+				"AT Expert User" => array(
+					"A user who has used this application before 
+					and knows how it works", 
+					"This user supports AT Seekers who need help 
+					using this application"
+				)
+			), 
+			"Panel System" => array(
+				"AT Seekers can now send their research notes 
+				for review by a panel. When they do, the 
+				research note becomes a case", 
+				"The panel and the AT Seeker can communicate with 
+				each other on the particular case"
+			),
+			"More to come...",
+		)) 
+		!!}
 	</div>
 	
 	<div class="container">
